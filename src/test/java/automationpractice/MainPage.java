5ab5501f48e42836a034fce85ed693cdde3c217b
dacresillvaant <mateusz.kubis94@gmail.com>
2022-05-22 14:00:07 +0200

    Creates Search class for Search test purposes.

--- src/test/java/automationpractice/MainPage.java
+++ src/test/java/automationpractice/MainPage.java
@@ -1,19 +1,26 @@
 package automationpractice;
 
 import driver.CustomisedDriver;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CheckElements {
+public class MainPage {
 
     ChromeDriver driver = new CustomisedDriver().setUp();
+
+    @AfterEach
+    public void closeAndQuit() {
+        driver.close();
+        driver.quit();
+    }
 
     @Test
     public void assertThatWebURLisCorrect() {
         driver.get("http://automationpractice.com/index.php");
-        String actualUrl = driver.getCurrentUrl();
         String expectedUrl = "http://automationpractice.com/index.php";
+        String actualUrl = driver.getCurrentUrl();
         Assertions.assertEquals(expectedUrl, actualUrl, "Actual URL is incorrect.");
     }
 }
---
+++ src/test/java/automationpractice/Search.java
@@ -0,0 +1,22 @@
+package automationpractice;
+
+import driver.CustomisedDriver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Search {
+
+    ChromeDriver driver = new CustomisedDriver().setUp();
+
+    @Test
+    public void assertThatSearchReturnsNoneResultsGivenTrololo() {
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElement(By.id("search_query_top")).sendKeys("Trololo");
+        driver.findElement(By.xpath("//*[contains(@name, 'submit_search')]")).click();
+        String expectedResults = "0 results have been found.";
+        String actualResults = driver.findElement(By.xpath("//*[contains(text(), 'results have been found')]")).getText();
+        Assertions.assertEquals(expectedResults, actualResults, "Amount of results is different than 0.");
+    }
+}
