de356a7f7bdc198fe3a33f844bd6e21cb9b6dea4
dacresillvaant <mateusz.kubis94@gmail.com>
2022-06-07 20:11:53 +0200

    Adds test which checks negative scenarios for newsletter submission.

--- src/test/java/automationpractice/MainPageTest.java
+++ src/test/java/automationpractice/MainPageTest.java
@@ -4,16 +4,20 @@ import driver.CustomisedDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 class MainPageTest {
 
-    ChromeDriver driver = new CustomisedDriver().setUp();
+    static ChromeDriver driver = new CustomisedDriver().setUp();
 
     @AfterEach
     public void closeAndQuit() {
-        driver.close();
-        driver.quit();
+//        driver.close();
+//        driver.quit();
     }
 
     @Test
@@ -24,5 +28,28 @@ class MainPageTest {
         Assertions.assertEquals(expectedUrl, actualUrl, "Actual URL is incorrect.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "@", "trololo", "emailmalpa.pl"})
+    void assertThatValidationIsShownForIncorrectEmailAddress(String input) {
+        //arrange
+        driver.get("http://automationpractice.com/index.php");
+        String newsletterLabelXPath = "//input[@id='newsletter-input']";
+        String newsletterButtonXPath = "//button[@name='submitNewsletter']";
+        String validationLabelXPath = "//p[@class = 'alert alert-danger']";
+        String expectedValidationMessage = "Newsletter : Invalid email address.";
+        String actualValidationMessage;
+        WebElement newsletterLabel = driver.findElement(By.xpath(newsletterLabelXPath));
+        WebElement newsletterButton = driver.findElement(By.xpath(newsletterButtonXPath));
+        WebElement validationLabel;
 
+        //act
+        newsletterLabel.sendKeys(input);
+        newsletterButton.click();
+        validationLabel = driver.findElement(By.xpath(validationLabelXPath));
+        actualValidationMessage = validationLabel.getText();
+
+        //assert
+        Assertions.assertEquals(expectedValidationMessage, actualValidationMessage);
+
+    }
 }
