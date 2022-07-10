4a9b2d02916ba35270a89fd0ede66dcce8b00172
dacresillvaant <mateusz.kubis94@gmail.com>
2022-07-10 18:14:31 +0200

    Imports Lombok, adds method which lets to wait for element to be clickable

--- src/main/java/pages/ItemsGrid.java
+++ src/main/java/pages/ItemsGrid.java
@@ -1,27 +1,37 @@
 package pages;
 
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
+@Getter
 public class ItemsGrid {
 
     public ItemsGrid(WebDriver driver) {
         PageFactory.initElements(driver, this);
         builder = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     private Actions builder;
+    private WebDriverWait wait;
 
     @FindBy(css = "div[class='product-container']")
     private List<WebElement> gridItems;
 
     @FindBy(css = "a[title='Add to cart']")
     private List<WebElement> addToCart;
+
+    @FindBy(css = "a[title='Proceed to checkout']")
+    private WebElement proceedToCheckout;
 
     public ItemsGrid printAmountOfItems() {
         System.out.println("Amount of items is: " + gridItems.size());
@@ -40,6 +50,16 @@ public class ItemsGrid {
 
     public ItemsGrid addToCartNthItem(int itemNumber) {
         addToCart.get(itemNumber).click();
+        return this;
+    }
+
+    public ItemsGrid proceedToCheckout() {
+        proceedToCheckout.click();
+        return this;
+    }
+
+    public ItemsGrid waitForIt(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
         return this;
     }
 
--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -10,8 +10,10 @@ public class TestClass extends TestBase {
     public void shouldOpenDesiredWebsite() {
         driver.get("http://automationpractice.com/index.php");
         ItemsGrid itemsGrid = new ItemsGrid(driver);
-//        itemsGrid.printAmountOfItems().clickNthItem(5);
-        itemsGrid.hoverOverNthItem(5).addToCartNthItem(5);
+        itemsGrid.hoverOverNthItem(5)
+                .addToCartNthItem(5)
+                .waitForIt(itemsGrid.getProceedToCheckout())
+                .proceedToCheckout();
     }
 
 }
